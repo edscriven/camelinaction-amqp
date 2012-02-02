851cc6b2045b1e1b5f9123a171af0589c7feb882
Ed Scriven <ed@poshhamster.com>
2012-02-02 16:34:22 +0000

    Converted RequestReplyJmsTest.java to RequestReplyAmqpTest.java

--- chapter7/amqp/src/test/java/camelinaction/RequestReplyAmqpTest.java
+++ chapter7/amqp/src/test/java/camelinaction/RequestReplyAmqpTest.java
@@ -16,24 +16,21 @@
  */
 package camelinaction;
 
-import static org.apache.camel.component.jms.JmsComponent.jmsComponentClientAcknowledge;
-
-import javax.jms.ConnectionFactory;
-
-import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 
-public class RequestReplyJmsTest extends CamelTestSupport {
+import amqp.spring.camel.component.SpringAMQPComponent;
+
+public class RequestReplyAmqpTest extends CamelTestSupport {
 
     protected CamelContext createCamelContext() throws Exception {
         CamelContext camelContext = super.createCamelContext();
 
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
-        camelContext.addComponent("jms", jmsComponentClientAcknowledge(connectionFactory));
-
+        camelContext.addComponent("spring-amqp", new SpringAMQPComponent(new CachingConnectionFactory("localhost")));
+        
         return camelContext;
     }
     
@@ -42,21 +39,21 @@ public class RequestReplyJmsTest extends CamelTestSupport {
             
             @Override
             public void configure() throws Exception {
-                from("jms:incomingOrders").inOut("jms:validate");                
-                from("jms:validate").bean(ValidatorBean.class);
+                from("spring-amqp::incomingOrders:incomingOrders").inOut("spring-amqp::validate");                
+                from("spring-amqp::validate:validate").bean(ValidatorBean.class);
             }
         };
     }
 
     @Test
     public void testClientGetsReply() throws Exception {
-        Object requestBody = template.requestBody("jms:incomingOrders", "<order name=\"motor\" amount=\"1\" customer=\"honda\"/>");
+    	Object requestBody = template.requestBody("spring-amqp::incomingOrders", "<order name=\"motor\" amount=\"1\" customer=\"honda\"/>");
         assertEquals("Valid", requestBody);        
     }    
 
     @Test
     public void testInvalidMessage() throws Exception {
-        Object requestBody = template.requestBody("jms:incomingOrders", "<order name=\"fork\" amount=\"1\" customer=\"honda\"/>");
+    	Object requestBody = template.requestBody("spring-amqp::incomingOrders", "<order name=\"fork\" amount=\"1\" customer=\"honda\"/>");
         assertEquals("Invalid", requestBody);        
     }    
 }
